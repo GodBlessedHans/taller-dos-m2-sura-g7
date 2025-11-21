a754389c2b655e0404a7d8244f340d762415e074
GodBlessedHans <haseveco@gmail.com>
2025-11-21 09:40:37 -0500

    Finished Exercise #2

--- hansvelasquez/script.js
+++ hansvelasquez/script.js
@@ -3,3 +3,9 @@ let puntos = [10, 20, 30, 40];
 let puntosDuplicados = puntos.map((punto) => punto * 2);
 console.log(puntos);
 console.log(puntosDuplicados);
+
+//Ejercicio 2: Lista de Nombres (map con objetos)
+let usuarios = [{id: 1, nombre: "Ana"}, {id: 2, nombre: "Luis"}, {id: 3, nombre: "Carlos"}];
+let nombres = usuarios.map((usuario) => usuario.nombre);
+console.log(nombres);
+
